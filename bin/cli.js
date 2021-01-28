0706a47f27cf4c3b2af285447b326c239875dbcc
woai3c <411020382@qq.com>
2021-01-28 22:56:22 +0800

    chore: add inquire

--- bin/cli.js
+++ bin/cli.js
@@ -1,10 +1,32 @@
 #!/usr/bin/env node
 const program = require('commander')
+const inquirer = require('inquirer')
 
 program
-  .option('-c, --cheese <type>', 'add the specified type of cheese', 'blue');
+    .version('0.1.0')
+    .command('create <appName>')
+    .description('create a new project powered by vue-cli-service')
+    .option('-t, --title', 'title to use before name')
+    .option('-d, --debug', 'display some debugging')
+    .action((appName, options) => {
+        console.log(appName, JSON.stringify(options))
+        inquirer
+            .prompt([
+                /* Pass your questions in here */
+                'test'
+            ])
+            .then(answers => {
+                // Use user feedback for... whatever!!
+            })
+            .catch(error => {
+                if(error.isTtyError) {
+                // Prompt couldn't be rendered in the current environment
+                } else {
+                // Something else went wrong
+                }
+            })
+    })
 
-program.parse();
 
-console.log(`cheese: ${program.opts().cheese}`);
-console.log(options, process.argv)
+
+program.parse()
