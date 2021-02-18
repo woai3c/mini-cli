2ea652b6610e60da1d7294141e7a01d1b646046c
woai3c <411020382@qq.com>
2021-02-18 15:26:11 +0800

    chore: 删除无效参数

--- bin/mv.js
+++ bin/mv.js
@@ -5,9 +5,7 @@ const create = require('../lib/create')
 program
     .version('0.1.0')
     .command('create <name>')
-    .description('create a new project powered by vue-cli-service')
-    .option('-t, --title', 'title to use before name')
-    .option('-d, --debug', 'display some debugging')
+    .description('create a new project')
     .action((name, options) => { 
         create(name, options)
     })
