dbf8db5ebd978574f812039e6be7ca30774ad2bf
woai3c <411020382@qq.com>
2021-02-19 22:07:31 +0800

    refactor: 删除用不到的代码以及为部分代码添加注释

--- bin/mv.js
+++ bin/mv.js
@@ -6,8 +6,8 @@ program
 .version('0.1.0')
 .command('create <name>')
 .description('create a new project')
-.action((name, options) => { 
-    create(name, options)
+.action(name => { 
+    create(name)
 })
 
 program.parse()
