caa56df354c1a59013ab164f8eaf7685bb0f303f
woai3c <411020382@qq.com>
2021-05-06 23:38:18 +0800

    chore: pre release sync

---
+++ scripts/release.js
@@ -0,0 +1,114 @@
+/**
+How to do a release:
+1. Make sure you have publish access for all packages:
+    - You must be in the CLI team in the npm @vue organization
+    - You must have publish access to vue-cli-version-marker
+    - Make sure you DO NOT have npm per-publish 2-factor / OTP enabled, as it
+      does not work with Lerna (which we use for batch publishing).
+2. Run `yarn release`, follow prompts
+3A. If everything works properly, the tag should have been auto-pushed and a
+    local changelog commit should have been generated. Go to 4.
+3B. If the publish fails half-way, things have gotten hairy. Now you need to
+    go to npm to check which packages have been published and manually publish
+    the ones that have not been published yet. After all have been published:
+    3B.1. Push the release git tag to GitHub.
+    3B.2. Run `yarn changelog` to generate changelog commit.
+4. Push the changelog commit to `dev` branch.
+5. Go to GitHub and verify that the changelog is live.
+6. Go to GitHub releases page and publish the release (this is required for
+   the release to show up in the issue helper)
+Note: eslint-config-* packages should be released separately & manually.
+*/
+
+process.env.VUE_CLI_RELEASE = true
+
+const execa = require('execa')
+const semver = require('semver')
+const inquirer = require('inquirer')
+const minimist = require('minimist')
+const { syncDeps } = require('./syncDeps')
+// const { buildEditorConfig } = require('./buildEditorConfig')
+
+const cliOptions = minimist(process.argv)
+const curVersion = require('../lerna.json').version
+
+const release = async () => {
+    console.log(`Current version: ${curVersion}`)
+
+    const bumps = ['patch', 'minor', 'major', 'prerelease']
+    const versions = {}
+    bumps.forEach(b => { versions[b] = semver.inc(curVersion, b) })
+    const bumpChoices = bumps.map(b => ({ name: `${b} (${versions[b]})`, value: b }))
+
+    const { bump, customVersion } = cliOptions['local-registry']
+        ? { bump: 'minor' }
+        : await inquirer.prompt([
+            {
+                name: 'bump',
+                message: 'Select release type:',
+                type: 'list',
+                choices: [...bumpChoices, { name: 'custom', value: 'custom' }],
+            },
+            {
+                name: 'customVersion',
+                message: 'Input version:',
+                type: 'input',
+                when: (answers) => answers.bump === 'custom',
+            },
+        ])
+
+    const version = customVersion || versions[bump]
+
+    const { yes } = cliOptions['local-registry']
+        ? { yes: true }
+        : await inquirer.prompt([
+            {
+                name: 'yes',
+                message: `Confirm releasing ${version}?`,
+                type: 'confirm',
+            },
+        ])
+
+    if (yes) {
+        await syncDeps({
+            version,
+            local: true,
+            skipPrompt: true,
+        })
+        delete process.env.PREFIX
+
+        // buildEditorConfig()
+
+        try {
+            await execa('git', ['add', '-A'], { stdio: 'inherit' })
+            await execa('git', ['commit', '-m', 'chore: pre release sync'], { stdio: 'inherit' })
+        } catch (e) {
+            // if it's a patch release, there may be no local deps to sync
+        }
+    }
+
+    let distTag = cliOptions['dist-tag'] || 'latest'
+    if (bump === 'prerelease' || semver.prerelease(version)) {
+        distTag = 'next'
+    }
+
+    const lernaArgs = [
+        'publish',
+        version,
+        '--dist-tag',
+        distTag,
+    ]
+    // keep all packages' versions in sync
+    lernaArgs.push('--force-publish')
+
+    if (cliOptions['local-registry']) {
+        lernaArgs.push('--no-git-tag-version', '--no-commit-hooks', '--no-push', '--yes')
+    }
+
+    await execa(require.resolve('lerna/cli'), lernaArgs, { stdio: 'inherit' })
+}
+
+release().catch(err => {
+    console.error(err)
+    process.exit(1)
+})
---
+++ scripts/syncDeps.js
@@ -0,0 +1,227 @@
+/* eslint-disable */
+// make sure generators are using the latest version of plugins,
+// and plugins are using the latest version of deps
+
+const fs = require('fs')
+const path = require('path')
+const chalk = require('chalk')
+const fetch = require('node-fetch')
+const semver = require('semver')
+const globby = require('globby')
+const { execSync } = require('child_process')
+const inquirer = require('inquirer')
+const readline = require('readline')
+
+const externalVueScopedPackages = {
+  '@vue/test-utils': true,
+  '@vue/eslint-config': true
+}
+const localPackageRE = /'(@vue\/(?:cli|eslint|babel)[\w-]+)': '\^([\w-.]+)'/g
+
+const versionCache = {}
+
+const getRemoteVersion = async (pkg) => {
+  if (versionCache[pkg]) {
+    return versionCache[pkg]
+  }
+  let res
+  try {
+    res = await fetch(`http://registry.npmjs.org/${pkg}/latest`).then(result => result.json())
+  } catch (e) {
+    return
+  }
+  versionCache[pkg] = res.version
+  return res.version
+}
+
+const getRemoteVersionSync = pkg => {
+  if (versionCache[pkg]) {
+    return versionCache[pkg]
+  }
+  const version = execSync(`npm view ${pkg} version`).toString().trim()
+  versionCache[pkg] = version
+  return version
+}
+
+const checkUpdate = (pkg, filePath, local, remote) => {
+  if (remote !== local) {
+    const isNewer = semver.gt(remote, local)
+    if (!isNewer) {
+      return false
+    }
+    const maybeBreaking = !semver.intersects(`^${local}`, `^${remote}`)
+    console.log(genUpdateString(pkg, filePath, local, remote, maybeBreaking))
+    return true
+  }
+}
+
+const checkUpdateAsync = async (pkg, filePath, local, remote) => {
+  if (remote !== local) {
+    const isNewer = semver.gt(remote, local)
+    if (!isNewer) {
+      return false
+    }
+    const maybeBreaking = !semver.intersects(`^${local}`, `^${remote}`)
+    if (!maybeBreaking) {
+      return true
+    }
+    const { shouldUpdate } = await inquirer.prompt([{
+      name: 'shouldUpdate',
+      type: 'confirm',
+      message: genUpdateString(pkg, filePath, local, remote, maybeBreaking) + `\n` +
+        `Update this dependency?`
+    }])
+    return shouldUpdate
+  }
+}
+
+function genUpdateString (pkg, filePath, local, remote, maybeBreaking) {
+  return `${chalk.cyan(pkg)}: ${local} => ${remote} ` +
+    (maybeBreaking ? chalk.red.bold(`maybe breaking `) : ``) +
+    chalk.gray(`(${path.relative(process.cwd(), filePath)})`)
+}
+
+const writeCache = {}
+const bufferWrite = (file, content) => {
+  writeCache[file] = content
+}
+const flushWrite = () => {
+  for (const file in writeCache) {
+    fs.writeFileSync(file, writeCache[file])
+  }
+}
+
+async function syncDeps ({ local, version, skipPrompt }) {
+  // 1. update all package deps
+  const updatedDeps = new Set()
+
+  if (!local) {
+    console.log('Syncing remote deps...')
+    const packages = await globby(['packages/@vue/*/package.json'])
+    const resolvedPackages = (await Promise.all(packages.filter(filePath => {
+      return filePath.match(/cli-service|cli-plugin|babel-preset|eslint-config/)
+    }).concat('package.json').map(async (filePath) => {
+      const pkg = require(path.resolve(__dirname, '../', filePath))
+      if (!pkg.dependencies) {
+        return
+      }
+      const deps = pkg.dependencies
+      const resolvedDeps = []
+      for (const dep in deps) {
+        if (dep.match(/^@vue/) && !externalVueScopedPackages[dep]) {
+          continue
+        }
+        let local = deps[dep]
+        if (local.charAt(0) !== '^') {
+          continue
+        }
+        local = local.replace(/^\^/, '')
+        readline.clearLine(process.stdout)
+        readline.cursorTo(process.stdout, 0)
+        process.stdout.write(dep)
+        const remote = await getRemoteVersion(dep)
+        resolvedDeps.push({
+          dep,
+          local,
+          remote
+        })
+      }
+      return {
+        pkg,
+        filePath,
+        resolvedDeps
+      }
+    }))).filter(_ => _)
+
+    for (const { pkg, filePath, resolvedDeps } of resolvedPackages) {
+      let isUpdated = false
+      for (const { dep, local, remote } of resolvedDeps) {
+        if (remote && await checkUpdateAsync(dep, filePath, local, remote)) {
+          pkg.dependencies[dep] = `^${remote}`
+          updatedDeps.add(dep)
+          isUpdated = true
+        }
+      }
+      if (isUpdated) {
+        bufferWrite(filePath, JSON.stringify(pkg, null, 2) + '\n')
+      }
+    }
+  }
+
+  console.log('Syncing local deps...')
+  const updatedRE = new RegExp(`'(${Array.from(updatedDeps).join('|')})': '\\^(\\d+\\.\\d+\\.\\d+[^']*)'`)
+  const paths = await globby(['packages/@vue/**/*.js'])
+  paths
+    .filter(p => !/\/files\//.test(p) && !/\/node_modules/.test(p))
+    .forEach(filePath => {
+      let isUpdated = false
+      const makeReplacer = versionGetter => (_, pkg, curVersion) => {
+        const targetVersion = versionGetter(pkg)
+        if (!targetVersion) return _
+        if (checkUpdate(pkg, filePath, curVersion, targetVersion)) {
+          isUpdated = true
+        }
+        return `'${pkg}': '^${targetVersion}'`
+      }
+
+      const localReplacer = makeReplacer(
+        pkg => {
+          try {
+            // for eslint-config-* packages, only use version field from package.json
+            // as they're published separately
+            if (pkg.includes('eslint-config')) {
+              return require(`../packages/${pkg}/package.json`).version
+            }
+
+            if (!semver.prerelease(version) && pkg.includes('@vue/cli-plugin')) {
+              return `${semver.major(version)}.${semver.minor(version)}.0`
+            }
+
+            // otherwise, inline version takes priority
+            return version || require(`../packages/${pkg}/package.json`).version
+          } catch (e) {}
+        }
+      )
+
+      const remoteReplacer = makeReplacer(getRemoteVersionSync)
+
+      const updated = fs.readFileSync(filePath, 'utf-8')
+        // update @vue packages in this repo
+        .replace(localPackageRE, localReplacer)
+        // also update vue, vue-template-compiler, vuex, vue-router
+        .replace(updatedRE, remoteReplacer)
+
+      if (isUpdated) {
+        bufferWrite(filePath, updated)
+      }
+    })
+
+  if (!Object.keys(writeCache).length) {
+    return console.log(`All packages up-to-date.`)
+  }
+
+  if (skipPrompt) {
+    flushWrite()
+    return
+  }
+
+  const { yes } = await inquirer.prompt([{
+    name: 'yes',
+    type: 'confirm',
+    message: 'Commit above updates?'
+  }])
+
+  if (yes) {
+    flushWrite()
+  }
+}
+
+exports.syncDeps = syncDeps
+
+if (!process.env.VUE_CLI_RELEASE) {
+  const args = require('minimist')(process.argv.slice(2))
+  syncDeps(args).catch(err => {
+    console.log(err)
+    process.exit(1)
+  })
+}
